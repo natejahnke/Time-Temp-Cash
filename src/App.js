60b8318016f00506c92a35559417c6f42cb94014
Nathan Jahnke <naterased@gmail.com>
2019-02-11 23:15:01 -0600

    added html5 geolocation function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './styles/styles.scss';
-import Card from './components/Card';
+// import Card from './components/Card';
+import HomeCard from './components/HomeCard';
+import HomeButton from './components/HomeButton';
 
 export default class App extends React.Component {
+  state = {
+    lat: null,
+    lng: null
+  };
+
+  getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      },
+      err => console.log(err)
+    );
+  };
+
   render() {
     return (
-      <div className='App'>
-        <h1>Test</h1>
-        <Card />
+      <div>
+        <button onClick={this.getLocation}>Click Me</button>
+        {/* <HomeButton onClick={this.getLocation} /> */}
+        <HomeCard />
       </div>
     );
   }
---
+++ src/components/HomeButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomeButton = props => (
+  <button href='#' className='action-button shadow animate blue'>
+    Locate Me
+  </button>
+);
+
+export default HomeButton;
